dba7095927ccc440bd0fa9495b9b0ed9ae679c98
KhalimulinDD <dima.khalimulin@mail.ru>
2024-11-06 21:31:34 +0300

    Fix fixtures

--- conftest.py
+++ conftest.py
@@ -7,38 +7,44 @@ from pages.collections_eco_friendly_page import CollectionsEcoFriendly
 
 @pytest.fixture()
 def create_account_page(page, context):
+    # Создаем объект страницы
     account_page = CreateAccount(page, context)
     yield account_page
-    # Снимаем скриншот перед закрытием страницы
+    # Получаем активную страницу и делаем скриншот
+    current_page = context.pages[-1]  # Берем последнюю открытую вкладку
     allure.attach(
-        page.screenshot(full_page=True),
+        current_page.screenshot(full_page=True),
         name="Screenshot",
         attachment_type=allure.attachment_type.PNG
     )
-    page.close()
+    current_page.close()
 
 
 @pytest.fixture()
 def collections_eco_friendly_page(page, context):
+    # Создаем объект страницы
     eco_friendly_page = CollectionsEcoFriendly(page, context)
     yield eco_friendly_page
-    # Снимаем скриншот перед закрытием страницы
+    # Получаем активную страницу и делаем скриншот
+    current_page = context.pages[-1]  # Берем последнюю открытую вкладку
     allure.attach(
-        page.screenshot(full_page=True),
+        current_page.screenshot(full_page=True),
         name="Screenshot",
         attachment_type=allure.attachment_type.PNG
     )
-    page.close()
+    current_page.close()
 
 
 @pytest.fixture()
 def sale_page(page, context):
+    # Создаем объект страницы
     sale = SalePage(page, context)
     yield sale
-    # Снимаем скриншот перед закрытием страницы
+    # Получаем активную страницу и делаем скриншот
+    current_page = context.pages[-1]  # Берем последнюю открытую вкладку
     allure.attach(
-        page.screenshot(full_page=True),
+        current_page.screenshot(full_page=True),
         name="Screenshot",
         attachment_type=allure.attachment_type.PNG
     )
-    page.close()
+    current_page.close()
